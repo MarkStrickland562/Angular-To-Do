354934cb2ea3cd27ad6d1d28f41dedd4b7da269c
Mark Strickland <mstrickland@globys.com>
2019-04-01 19:40:08 -0700

    Add initial project files.

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3) {
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+}
